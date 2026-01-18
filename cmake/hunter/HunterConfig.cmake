c5a3e0e181623eaf1025fb9271f4a2842c04664e
Kevin Scroggins <nitro404@gmail.com>
2026-01-18 16:05:58 -0500

    Add CMake support

---
+++ cmake/hunter/HunterConfig.cmake
@@ -0,0 +1,7 @@
+include_guard()
+
+hunter_config(scintilla
+	URL "https://github.com/Telefrag-Software/scintilla/archive/refs/heads/hunter-wx-5.0.0.1.zip"
+	VERSION "5.0.0.1"
+	SHA1 "d390c96a0c02e879732e5c41e4783ede438f2952"
+)
